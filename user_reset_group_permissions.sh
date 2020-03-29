414114b5f5ee1385c3bbf8b94ed503b2eecb06e9
Patricki <pstambaugh14@gmail.com>
2020-03-29 09:56:21 -0400

    Added better notes
    #This script is intended to emulate a logoff followed by a login by the user executing the script. 
    #This comes in handy when there are situations in which something like 'sudo usermod -a -G <group> <$USER>' 
    #doesn't reflect in accordance with the live Operating System's configurations globally (i.e. - when a 
    #reboot, logoff or whatever is necessary to interact with another element, application, setting, etc.).

--- user_reset_group_permissions.sh
+++ user_reset_group_permissions.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+#This script is intended to emulate a logoff followed by a login by the user executing the script. 
+#This comes in handy when there are situations in which something like 'sudo usermod -a -G <group> <$USER>' 
+#doesn't reflect in accordance with the live Operating System's configurations globally (i.e. - when a 
+#reboot, logoff or whatever is necessary to interact with another element, application, setting, etc.).
+
 exec su -l $USER
