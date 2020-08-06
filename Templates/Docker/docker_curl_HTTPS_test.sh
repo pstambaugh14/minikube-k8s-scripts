2b49468fe2a4036db8727a679af9e6f348990b16
Patrick Stambaugh <17075207+pstambaugh14@users.noreply.github.com>
2020-08-06 12:29:07 -0400

    Create docker_curl_HTTPS_test.sh

---
+++ Templates/Docker/docker_curl_HTTPS_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This is used to check for API connectivity once HTTPS is implemented.
+
+curl https://<hostname>:2376/images/json \
+  --cert ~/.docker/certs/cert.pem \
+  --key ~/.docker/certs/key.pem \
+  --cacert ~/.docker/certs/ca.pem
