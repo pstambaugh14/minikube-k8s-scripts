165fc1c8a997e438bf5342ffeaf5536740d80468
Patrick Stambaugh <17075207+pstambaugh14@users.noreply.github.com>
2020-08-06 12:36:00 -0400

    Create docker_curl_test.sh

---
+++ Templates/Docker/docker_curl_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This test is used to check Docker API connectivity and return some general configuration settings.
+
+#WORKING:
+curl -X GET "http(s)://<host_name>:<port>/v1.40/containers/json?all=false&size=false" -H  "accept: application/json" \
+--cert ~/.docker/certs/cert.pem --key ~/.docker/certs/key.pem --cacert ~/.docker/certs/ca.pem
+
+
+#Also WORKING (if no TLS, if API/Docker Daemon also still on UNIX Socket Listening)
+curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" \
+  -d '"{"status": ["running"]}"' \
+  -X GET http:/v1.40/containers/json
