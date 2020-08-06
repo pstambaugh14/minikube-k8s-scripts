b804cc46166601a56ee5664cc4326445e32d7a3e
Patrick Stambaugh <17075207+pstambaugh14@users.noreply.github.com>
2020-08-06 12:10:19 -0400

    Update jenkins-cli-connect.sh

--- Templates/Jenkins/jenkins-cli-connect.sh
+++ Templates/Jenkins/jenkins-cli-connect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar jenkins-cli.jar -s http://gremio-desktop:8080/ -auth Jenkins:112e9e6b9827b1aa7d9f96b921b1cfcc33
+java -jar jenkins-cli.jar -s http://localhost:8080/ -auth Jenkins:<hex_pw>
