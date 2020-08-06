01a4e96489090d60c3feef1b344d4695ae44305f
Patrick Stambaugh <17075207+pstambaugh14@users.noreply.github.com>
2020-08-06 12:09:14 -0400

    Update docker-minikube-env-vars.sh

--- Templates/Jenkins/docker-minikube-env-vars.sh
+++ Templates/Jenkins/docker-minikube-env-vars.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 $ eval $(minikube docker-env)
 
-#[patrick@gremio-desktop Documents]$ minikube docker-env
-#export DOCKER_TLS_VERIFY="1"
-#export DOCKER_HOST="tcp://192.168.39.153:2376"
-#export DOCKER_CERT_PATH="/home/patrick/.minikube/certs"
-## Run this command to configure your shell:
-## eval $(minikube docker-env)
-#[patrick@gremio-desktop Documents]$ eval $(minikube docker-env)
-
